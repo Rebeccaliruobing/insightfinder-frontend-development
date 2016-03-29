31f81d5c65a719dd10ead454e67bf23cfa4a1731
Hao Fu <nullnoid@gmail.com>
2016-03-29 09:42:10 +0800

    [IMP] Add google project

--- frontend/components/monitoring/projects/google.js
+++ frontend/components/monitoring/projects/google.js
@@ -1,11 +1,81 @@
 import React from 'react';
-import _ from 'lodash';
+import GoogleProjectModal from './google_modal';
 
+class GoogleProjects extends React.Component {
 
-class ProjectsGoogle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false
+    };
+  }
+
+  handleModalClose = () => this.setState({showModal: false});
+
   render() {
-    return <div>Google </div>
+    return (
+      <div>
+        <table className="ui striped table unstackable">
+          <thead className="full-width">
+          <tr>
+            <th colSpan="6">
+              <div className="ui left floated small primary labeled icon button"
+                   onClick={(e) => this.setState({showModal: true})}>
+                <i className="plus icon" />Register
+              </div>
+              <div className="ui left floated small labeled icon button">
+                <i className="remove icon" />Remove
+              </div>
+            </th>
+          </tr>
+          <tr>
+            <th className="collapsing">
+              <div className="ui fitted checkbox">
+                <input type="checkbox" /><label></label>
+              </div>
+            </th>
+            <th>Project Name</th>
+            <th>Project Id</th>
+            <th>Project Type</th>
+            <th>Service Account Email</th>
+            <th>.p12 key file</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td className="collapsing">
+              <div className="ui fitted checkbox">
+                <input type="checkbox" /><label></label>
+              </div>
+            </td>
+            <td>test1</td>
+            <td></td>
+            <td></td>
+            <td>GAE</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="collapsing">
+              <div className="ui fitted checkbox">
+                <input type="checkbox" /><label></label>
+              </div>
+            </td>
+            <td>test2</td>
+            <td></td>
+            <td></td>
+            <td>GCE</td>
+            <td></td>
+          </tr>
+          </tbody>
+        </table>
+        {
+          this.state.showModal &&
+          <GoogleProjectModal onClose={this.handleModalClose}/>
+        }
+      </div>
+    )
   }
 }
 
-export default ProjectsGoogle;
+export default GoogleProjects;
---
+++ frontend/components/monitoring/projects/google_modal.js
@@ -0,0 +1,67 @@
+import $ from 'jquery';
+import React from 'react';
+import {Modal} from '../../../artui';
+
+
+class GoogleProjectModal extends React.Component {
+  constructor(props){
+    super(props);
+    this._dropdown = null;
+  }
+
+  componentDidMount() {
+    if(this._dropdown){
+      $(this._dropdown).dropdown();
+    }
+  }
+
+  render(){
+    return (
+      <Modal {...this.props} size="small" closable={false}>
+        <Modal.Content>
+          <form className="ui form">
+            <div className="field">
+              <label>Project Name</label>
+              <input type="text" name="name" />
+            </div>
+            <div className="field">
+              <label>Project ID</label>
+              <input type="text" name="project_id" />
+            </div>
+            <div className="field">
+              <label>Project Type</label>
+              <div className="ui selection dropdown" ref={c => this._dropdown = c}>
+                <input name="type" type="hidden" value="0,1" />
+                <i className="dropdown icon" />
+                <span className="default text" />
+                <div className="menu">
+                  <div className="item">GAE</div>
+                  <div className="item">GCE</div>
+                </div>
+              </div>
+            </div>
+            <div className="field">
+              <label>Service Account Email</label>
+              <input type="text" name="email" />
+            </div>
+            <div className="field">
+              <label>.p12 key file</label>
+              <input type="text" name="p12" />
+            </div>
+          </form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Modal.ActionButton deny={true}>Cancel</Modal.ActionButton>
+          <Modal.ActionButton
+            approve={true} className="labeled">
+            <div className="ui button orange">
+              <i className="save icon"></i>Register
+            </div>
+          </Modal.ActionButton>
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+}
+
+export default GoogleProjectModal;
