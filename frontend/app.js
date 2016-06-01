09d838ea3d57413a3bfbb16dabbcc603e467548f
Nullnoid <nullnoid@gmail.com>
2016-06-01 15:54:02 +0800

    [REVIEW] code review to understand liang changes

--- frontend/app.js
+++ frontend/app.js
@@ -2,24 +2,18 @@ import './app.less';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory, useRouterHistory, IndexRoute, IndexRedirect} from 'react-router';
+import {Router, Route, browserHistory, 
+  useRouterHistory, IndexRoute, IndexRedirect} from 'react-router';
 import {createHashHistory} from 'history'
-import {Console, Link} from './artui/react';
-
 import store from 'store';
+
+import {Console, Link} from './artui/react';
 
 import Login from './components/auth/login'
 import {cloudRoute} from './components/cloud';
 import {settingsRoute} from './components/settings';
 import ProjectDetails from './components/cloud/monitoring/details';
 import apis from './apis';
-
-
-const EmptyContent = function (props) {
-  return (
-    <Console.Content style={{height: 1000}}>Hello Insightfinder!</Console.Content>
-  )
-};
 
 class App extends React.Component {
   static contextTypes = {
@@ -43,6 +37,7 @@ class App extends React.Component {
       dashboardUservalues,
       dashboardDailySummaryReport
     } = this.context;
+    
     let loading = !(_.keys(userInstructions).length > 0 && _.keys(dashboardUservalues).length > 0);
 
     return (
@@ -70,6 +65,7 @@ class App extends React.Component {
   }
 }
 
+// Live Monitoring project detail page
 const liveMonitoringApp = function(props) {
   let {location, params} = props;
   return (
@@ -81,7 +77,6 @@ const liveMonitoringApp = function(props) {
 };
 
 const appHistory = useRouterHistory(createHashHistory)({queryKey: false});
-
 const routes = (
   <Router history={appHistory}>
     <Route component={App} path="/">
@@ -133,11 +128,17 @@ class AppRoute extends React.Component {
   }
 
   loadData() {
-    if (!(store.get('userName') && store.get('token'))) return;
+    // Load data only when user login
+    if (!(store.get('userName') && store.get('token'))) {
+      return;
+    }
+
     apis.getUserInstructions().then((resp)=> {
       this.setState({userInstructions: resp});
     });
+    
     apis.postDashboardUserValues(store.get('userName')).then((resp)=> {
+      
       resp.dataAllInfo = JSON.parse(resp.dataAllInfo);
       resp.extServiceAllInfo = JSON.parse(resp.extServiceAllInfo);
       resp.incidentAllInfo = JSON.parse(resp.incidentAllInfo);
@@ -156,8 +157,10 @@ class AppRoute extends React.Component {
           metaData: JSON.parse(info.metaData)
         });
       });
+      
       this.setState({dashboardUservalues: resp});
     });
+    
     // apis.postDashboardDailySummaryReport(store.get('userName')).then((resp)=> {
     //   this.setState({dashboardDailySummaryReport: resp});
     // });
