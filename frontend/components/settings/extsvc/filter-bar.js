517e82dfadde5baa341edf9346d7a81d6a8f48cf
Chao Huang <chaoh818@gmail.com>
2016-06-16 10:36:49 -0400

    外部服务的第一版

---
+++ frontend/components/settings/extsvc/filter-bar.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import moment from 'moment';
+import {Link, IndexLink} from 'react-router';
+
+import {Console, ButtonGroup, Button, Dropdown, Accordion, Message} from '../../../artui/react';
+import {
+  ProjectSelection,
+  ModelType,
+  WindowWithWeek,
+} from '../../selections';
+
+import DateTimePicker from "../../ui/datetimepicker/index";
+
+export default  class FilterBar extends Component {
+  static contextTypes = {
+    userInstructions: React.PropTypes.object,
+    dashboardUservalues: React.PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectName: undefined,
+      projectType: undefined,
+      weeks: '1',
+      startTime: moment().toDate(),
+      endTime: moment().add(-1, 'w').toDate()
+    };
+  }
+
+  componentDidMount() {
+    let projects = (this.context.dashboardUservalues || {}).projectSettingsAllInfo || [];
+    if (projects.length > 0) this.handleProjectChange(projects[0].projectName, projects[0].projectName);
+  }
+
+  handleProjectChange(value, projectName) {
+    let {projectString, incidentAllInfo, dataAllInfo} = this.context.dashboardUservalues;
+    let project = projectString.split(',').map((s)=>s.split(":")).find(([name]) => name == projectName);
+    // 前三部分是名称，数据类型dataType和云类型cloudType
+    let [name, dataType, cloudType] = project;
+    let update = {projectName};
+    switch (dataType) {
+      case 'AWS':
+        update.projectType = "AWS/CloudWatch";
+        break;
+      case 'GAE':
+        update.projectType = `${dataType}/CloudMonitoring`;
+        break;
+      case 'GCE':
+        update.projectType = `${dataType}/CloudMonitoring`;
+        break;
+      default:
+        update.projectType = `${cloudType}/Agent`;
+    }
+    this.setState(update);
+  }
+
+  handleEndTimeChange(endTime) {
+    let {weeks} = this.state;
+    this.setState({
+      startTime: moment(endTime).add(-weeks, 'w').toDate(),
+      endTime
+    })
+  }
+
+  handleSubmit() {
+    this.props.onSubmit && this.props.onSubmit(this.state);
+  }
+
+  render() {
+    const {projectName, startTime, endTime, projectType} = this.state;
+    const labelStyle = {};
+
+    return (
+      <div className="ui form">
+        <div className="two fields fill">
+          <div className="field">
+            <label style={labelStyle}>PagerDuty Settings</label>
+            You can manage your alerts with your PagerDuty account. Click the button below to integrate your account alerts with PagerDuty.
+          </div>
+          <div className="field">
+            <a href="https://connect.pagerduty.com/connect?vendor=ad2938f1f094d65d86fa&callback=https://app.insightfinder.com/service-integration"><img alt="Connect_button" src="/oldstatic/pd-connect.png" /></a>
+          </div>
+        </div>
+
+        <div className="ui field">
+          <Button className="orange" onClick={this.handleSubmit.bind(this)}>Submit</Button>
+        </div>
+      </div>
+    )
+  }
+}
