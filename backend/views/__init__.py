275ac167ea6212566b151334c96a4356d4ceeaca
Nullnoid <nullnoid@gmail.com>
2016-07-09 00:04:54 +0800

    remove useless backend code

--- backend/views/__init__.py
+++ backend/views/__init__.py
@@ -8,18 +8,10 @@ from flask import jsonify, request
 def init_app(app):
 
     from .main import MainView
-    from .auth import AuthView
-    from .liveMonitoring import LiveMonitoringView
-    from . import apis
     main_view = MainView.as_view(b'main_view')
-    auth_view = AuthView.as_view(b'auth_view')
-
-    app.add_url_rule('/auth/', view_func=auth_view)
-    app.add_url_rule('/auth/<string:action>', view_func=auth_view)
 
     app.add_url_rule('/', view_func=main_view)
     app.add_url_rule('/<path:path>', defaults={'action': 'all'}, view_func=main_view)
-    app.add_url_rule('/liveMonitoring', view_func=main_view)
 
     @app.route("/upload", methods=['POST'])
     def upload():
--- backend/views/liveMonitoring.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from flask import render_template
-from ..core import ActionView
-
-
-class LiveMonitoringView(ActionView):
-    def index(self):
-        return render_template('liveMonitoring.html')
