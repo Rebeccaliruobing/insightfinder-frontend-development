b5c4ab63a03b38ae868625f5543ceb69f6f977e8
Nullnoid <nullnoid@gmail.com>
2016-07-11 13:49:54 +0800

    adjust topbar style

--- frontend/components/cloud/navbar.js
+++ frontend/components/cloud/navbar.js
@@ -7,23 +7,23 @@ export default function (props) {
     <Console.Navbar>
       <div className="ui vertical menu text icon-menu">
         <Link to="/cloud/monitoring" className="item text-white">
-          <i className="line chart icon"></i>
+          <i className="line chart icon"/>
           <span> Anomaly Detection</span>
         </Link>
         <Link to="/cloud/incident-analysis" className="item text-white">
-          <i className="zoom icon"></i>
+          <i className="zoom icon"/>
           <span> Triage</span>
         </Link>
         <Link to="/cloud/outlier-detection" className="item text-white">
-          <i className="icon icon-icons01 iconfont "></i>
+          <i className="icon icon-icons01 iconfont "/>
           <span> Cluster Outlier Detection</span>
         </Link>
         <Link to="/cloud/rollout-check" className="item text-white">
-          <i className="checkmark box icon"></i>
+          <i className="checkmark box icon"/>
           <span> Behavior Change Detection</span>
         </Link>
         <Link to="/cloud/summary-report" className="item text-white">
-          <i className="icon icon-report iconfont"></i>
+          <i className="icon icon-report iconfont"/>
           <span> Summary Report</span>
         </Link>
       </div>
