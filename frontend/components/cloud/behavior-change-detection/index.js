6330adbf552e35b63d3bde10b0057c280053d4f9
Hao Fu <nullnoid@gmail.com>
2016-08-05 16:26:54 +0800

    Remove useless code

--- frontend/components/cloud/behavior-change-detection/index.js
+++ frontend/components/cloud/behavior-change-detection/index.js
@@ -1,47 +1,18 @@
 import React, {Component} from 'react';
-import store from 'store';
 import {Link, IndexLink} from 'react-router';
-import RcSlider from '../../ui/rc-slider';
-import HeatMapCard from '../../ui/heat-map-card';
-import {
-    Modal, Console, ButtonGroup, Button, Popup, Dropdown, Accordion, Message
-} from '../../../artui/react/index';
-import {Dygraph} from '../../../artui/react/dataviz';
-
-import apis from '../../../apis';
-
-import moment from 'moment';
+import {Console} from '../../../artui/react/index';
 import OutlierDetection from '../outlier-detection';
 import RolloutCheck from '../rollout-check';
 
 export default class BehaviorChangeDetection extends Component {
-    static contextTypes = {
-        userInstructions: React.PropTypes.object
-    };
-
     constructor(props) {
         super(props);
-        let weeks = 1;
         this.state = {
-            view: 'chart',
-            dateIndex: 0,
-            timeIndex: 0,
-            showAddPanel: true,
-            params: {
-                projects: [],
-                weeks: weeks,
-                endTime: moment(new Date()).toDate(),
-                startTime: moment(new Date()).add(-7 * weeks, 'days')
-            },
-            data: {},
             tabStates: {
                 metric: 'active',
                 instance: ''
             },
         };
-    }
-
-    componentDidMount() {
     }
 
     selectTab(e, tab) {
@@ -50,73 +21,40 @@ export default class BehaviorChangeDetection extends Component {
             return '';
         });
         tabStates[tab] = 'active';
-        this.setState({tabStates: tabStates});
-    }
-
-    handleData(data) {
-        this.setState({data: data}, ()=> {
-            this.setHeatMap(_.keys(this.state.data.modelData)[1]);
-        })
-    }
-
-    handleToggleFilterPanel() {
-        this.setState({showAddPanel: !this.state.showAddPanel}, ()=> {
-            this.state.showAddPanel ? this.$filterPanel.slideDown() : this.$filterPanel.slideUp()
-        })
+        this.setState({ tabStates: tabStates });
     }
 
     render() {
-        const self = this;
-        const {showAddPanel, tabStates} = this.state;
-        const {userInstructions} = this.context;
-        const panelIconStyle = showAddPanel ? 'angle double up icon' : 'angle double down icon';
-        // onClick={this.handleToggleFilterPanel.bind(this)}
+        const { tabStates } = this.state;
         return (
             <Console.Content>
                 <div className="ui main tiny container" ref={c => this._el = c}>
-                    <div className="ui clearing vertical segment">
-                        <ButtonGroup className="right floated basic icon">
-                            <Button>
-                                <Popup position="bottom right">
-                                    <i className={panelIconStyle}/>
-                                    <span className="ui mini popup">Expand & Close</span>
-                                </Popup>
-                            </Button>
-                            <Button>
-                                <i className="setting icon"/>
-                            </Button>
-                        </ButtonGroup>
+                    <div className="ui vertical segment">
+                        <div>
+                            <div className="ui pointing secondary menu">
+                                <a className={tabStates['metric'] + ' item'}
+                                   onClick={(e) => this.selectTab(e, 'metric')}>By Metric</a>
+                                <a className={tabStates['instance'] + ' item'}
+                                   onClick={(e) => this.selectTab(e, 'instance')}>By Instance</a>
+                            </div>
+
+                            <div className={tabStates['metric'] + ' ui tab '}>
+                                {tabStates['metric'] === 'active' ?
+                                    <div>
+                                        <RolloutCheck />
+                                    </div>
+                                    : null}
+                            </div>
+                            <div className={tabStates['instance'] + ' ui tab '}>
+                                {tabStates['instance'] === 'active' ?
+                                    <div>
+                                        <OutlierDetection />
+                                    </div>
+                                    : null}
+                            </div>
+
+                        </div>
                     </div>
-                    <div className="ui vertical segment filterPanel"
-                             ref={(c)=>this.$filterPanel = $(ReactDOM.findDOMNode(c))}>
-                            <i className="close link icon" style={{float:'right', marginTop: '-10px'}}
-                               onClick={this.handleToggleFilterPanel.bind(this)}/>
-
-                    <div>
-                        <div className="ui pointing secondary menu">
-                            <a className={tabStates['metric'] + ' item'}
-                               onClick={(e) => this.selectTab(e, 'metric')}>By Metric</a>
-                            <a className={tabStates['instance'] + ' item'}
-                               onClick={(e) => this.selectTab(e, 'instance')}>By Instance</a>
-                        </div>
-
-                        <div className={tabStates['metric'] + ' ui tab '}>
-                            {tabStates['metric'] === 'active' ?
-                                <div>
-                                    <RolloutCheck />
-                                </div>
-                                : null}
-                        </div>
-                        <div className={tabStates['instance'] + ' ui tab '}>
-                            {tabStates['instance'] === 'active' ?
-                                <div>
-                                    <OutlierDetection />
-                                </div>
-                                : null}
-                        </div>
-
-                    </div>
-                </div>
                 </div>
             </Console.Content>
         );
