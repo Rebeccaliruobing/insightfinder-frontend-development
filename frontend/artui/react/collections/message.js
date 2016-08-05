220b9ad9cfbd744b5cae97e08329dd640b0c235a
Hao Fu <nullnoid@gmail.com>
2016-08-05 17:07:02 +0800

    fix userinstructions not display issue

--- frontend/artui/react/collections/message.js
+++ frontend/artui/react/collections/message.js
@@ -1,7 +1,7 @@
 /*
  * Message提供了对SemanticUI message的简单封装.
  * http://semantic-ui.com/collections/message.html
-**/
+ **/
 
 import $ from 'jquery';
 import React from 'react';
@@ -9,7 +9,7 @@ import classNames from 'classnames';
 import ReactTimeout from 'react-timeout';
 
 class Message extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this._$el = null;
@@ -23,7 +23,7 @@ class Message extends React.Component {
       }
     });
   }
-  
+
   componentDidMount() {
     if (this._$el) {
       if (this.props.closable) {
@@ -32,7 +32,7 @@ class Message extends React.Component {
             this._close();
           });
       }
-      
+
       if (this.props.autoDismiss) {
         this.props.setTimeout(() => {
           this._close();
@@ -40,38 +40,44 @@ class Message extends React.Component {
       }
     }
   }
-  
+
   componentWillUnmount() {
     if (this._$el && this.props.closable) {
       this._$el.find('.close').off('click');
     }
   }
-    
+
   render() {
-    let {tag, className, 
-      closable, onClose, 
-      autoDismiss, dismissTimeout,
-      children, style, ...others} = this.props;
+    let {
+      tag, className,
+      closable, onClose,
+      autoDismiss, dismissTimeout, dangerouslySetInnerHTML,
+      children, style, ...others
+    } = this.props;
     return React.createElement(tag, {
       ref: c => this._$el = $(c),
       className: classNames('ui', className, 'message'),
-      style: style,
+      style,
+      dangerouslySetInnerHTML,
     }, children);
   }
-};
+}
+;
 
 Message.propTypes = {
   tag: React.PropTypes.string,
   closable: React.PropTypes.bool,
   onClose: React.PropTypes.func,
   autoDismiss: React.PropTypes.bool,
-  dismissTimeout: React.PropTypes.number  // seconds
+  dismissTimeout: React.PropTypes.number,  // seconds
+  dangerouslySetInnerHTML: React.PropTypes.object,
 };
 
 Message.defaultProps = {
   tag: 'div',
   closable: false,
-  onClose: () => {},
+  onClose: () => {
+  },
   autoDismiss: false,
   dismissTimeout: 10
 };
