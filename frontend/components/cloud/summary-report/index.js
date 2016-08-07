a42d8e505e1c4dd308a807fa4447453490c09c1e
Hao Fu <nullnoid@gmail.com>
2016-08-07 12:07:05 +0800

    Remove useless code

--- frontend/components/cloud/summary-report/index.js
+++ frontend/components/cloud/summary-report/index.js
@@ -1,59 +1,45 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
-import {Link, IndexLink} from 'react-router';
+import {autobind} from 'core-decorators';
 import _ from 'lodash';
-import store from 'store';
-import {Console, ButtonGroup, Button, Popup, Dropdown, Accordion, Message} from '../../../artui/react';
+import {Console, ButtonGroup, Button, Popup, Message} from '../../../artui/react';
 import FilterBar from './filter-bar';
 import apis from '../../../apis';
 import "./summaryReport.less";
-import ReactD3 from 'react-d3-components';
-var BarChart = ReactD3.BarChart;
+const colorMapLow = require('../../../images/color-map-low.png');
 
 export default class RenderSummaryReport extends Component {
     constructor(props) {
         super(props);
         let today = new Date();
-        let anomalyNumList = _.slice(this.props.summaryData.anomalyNumList,0,30);
-        if(anomalyNumList.length>0&&anomalyNumList.length<7){
-            for (let i=anomalyNumList.length;i<7;i++){
+        let anomalyNumList = _.slice(this.props.summaryData.anomalyNumList, 0, 30);
+        if (anomalyNumList.length > 0 && anomalyNumList.length < 7) {
+            for (let i = anomalyNumList.length; i < 7; i++) {
                 anomalyNumList.push(0);
             }
         }
         this.state = {
             summaryData: this.props.summaryData || {},
             createDate: this.props.createDate || "",
-            userName: store.get('userName'),
             anomalyNumList: anomalyNumList,
-            barChartList: anomalyNumList.map(function (value,index) {
+            barChartList: anomalyNumList.map(function (value, index) {
                 today = new Date(moment(today).subtract(1, 'days'));
-                let month = today.getMonth()+1;
+                let month = today.getMonth() + 1;
                 let day = today.getDate();
-                return  {x: month+"/"+day, y: value}
+                return { x: month + "/" + day, y: value }
             })
         };
     }
 
-    componentDidMount() {
-    }
-
-    rand(min, max, num) {
-        var rtn = [];
-        while (rtn.length < num) {
-            rtn.push((Math.random() * (max - min)) + min);
-        }
-        return rtn;
-    }
-
     render() {
-        let {summaryData, userName, createDate,barChartList,anomalyNumList} = this.state;
+        let { summaryData, barChartList, anomalyNumList } = this.state;
         let hasSummary;
-        if(summaryData['anomalyDetails'].length){
+        if (summaryData['anomalyDetails'].length) {
             hasSummary = true;
         }
         let hasMetrics;
-        if(summaryData['metricStats'].length){
+        if (summaryData['metricStats'].length) {
             hasMetrics = true;
         }
         let instanceListNumber = {};
@@ -86,19 +72,19 @@ export default class RenderSummaryReport extends Component {
         anomalyNumList = _.reverse(anomalyNumList);
         return (
             <div>
-                <div style={{'marginBottom': '16px'}}>
+                <div style={{ 'marginBottom': '16px' }}>
                     <div>Anomaly number time series: [{(anomalyNumList || []).map(function (value, index) {
                         return value + (index == anomalyNumList.length - 1 ? "" : ", ")
                     })}]
                     </div>
-                    {hasMetrics && 
+                    {hasMetrics &&
                     <div>
                         <span>Basic metric value statistics for {(summaryData['metricStats'] || []).length}
-                             metrics</span>
+                            metrics</span>
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={summaryData['metricTableColormap']}/>
                     </div>
                     }
-                    {hasMetrics && 
+                    {hasMetrics &&
                     <div>
                         <table className="metric-table">
                             <tbody>
@@ -163,7 +149,7 @@ export default class RenderSummaryReport extends Component {
                                         <td>
                                             {value['Anomaly Relevance Score']}
                                         </td>
-                                        <td style={{'backgroundColor': value['Status']}}>
+                                        <td style={{ 'backgroundColor': value['Status'] }}>
                                         </td>
                                     </tr>
                                 )
@@ -176,28 +162,18 @@ export default class RenderSummaryReport extends Component {
                     </div>
                     }
                     {hasSummary &&
-                    <div style={{'marginTop': '16px'}}>
+                    <div style={{ 'marginTop': '16px' }}>
                         <div><span>Anomaly summary in the past 24 hours: &nbsp;&nbsp;&nbsp;<img
-                            src='/oldstatic/color-map-normal.png'/></span></div>
+                            src={colorMapLow}/></span></div>
                         <div>
                             <table className="anomaly-table">
                                 <tbody>
                                 <tr>
-                                    <td>
-                                        ID
-                                    </td>
-                                    <td>
-                                        Start Time
-                                    </td>
-                                    <td>
-                                        Duration (min)
-                                    </td>
-                                    <td>
-                                        Avg Anomaly Degree
-                                    </td>
-                                    <td>
-                                        Status
-                                    </td>
+                                    <td>ID</td>
+                                    <td>Start Time</td>
+                                    <td>Duration (min)</td>
+                                    <td>Avg Anomaly Degree</td>
+                                    <td>Status</td>
                                 </tr>
                                 {(summaryData['anomalySummary'] || []).map(function (value, index) {
                                     return (
@@ -214,7 +190,7 @@ export default class RenderSummaryReport extends Component {
                                             <td>
                                                 {value['Avg Anomaly Degree']}
                                             </td>
-                                            <td style={{'backgroundColor': value['Status']}}>
+                                            <td style={{ 'backgroundColor': value['Status'] }}>
                                             </td>
                                         </tr>
                                     )
@@ -230,28 +206,13 @@ export default class RenderSummaryReport extends Component {
     }
 }
 export default class SummaryReport extends Component {
-    static contextTypes = {
-        userInstructions: React.PropTypes.object,
-        dashboardUservalues: React.PropTypes.object
-    };
-
     constructor(props) {
         super(props);
-        let weeks = 1;
         this.state = {
-            view: 'chart',
-            dateIndex: 0,
-            timeIndex: 0,
             summaryReport: '',
             showAddPanel: true,
-            filterBar: {projectName: ''},
+            filterBar: { projectName: '' },
             loading: true,
-            params: {
-                projects: [],
-                weeks: weeks,
-                endTime: moment(new Date()).toDate(),
-                startTime: moment(new Date()).add(-7 * weeks, 'days')
-            }
         };
     }
 
@@ -263,7 +224,8 @@ export default class SummaryReport extends Component {
         $('#loadingRefresh').addClass('loading');
         this.handleGetSummaryData(true);
     }
-    handleGetSummaryData(forceReload){
+
+    handleGetSummaryData(forceReload) {
         let self = this;
         apis.postDashboardDailySummaryReportNew(forceReload).then((resp) => {
             $('#loadingRefresh').removeClass('loading');
@@ -272,8 +234,9 @@ export default class SummaryReport extends Component {
         }).catch(()=> {
         });
     }
+
     handleToggleFilterPanel() {
-        this.setState({showAddPanel: !this.state.showAddPanel}, ()=> {
+        this.setState({ showAddPanel: !this.state.showAddPanel }, ()=> {
             this.state.showAddPanel ? this.$filterPanel.slideDown() : this.$filterPanel.slideUp()
         })
     }
@@ -285,11 +248,11 @@ export default class SummaryReport extends Component {
         //}).catch(()=> {
         //
         //});
-        this.setState({filterBar: data});
+        this.setState({ filterBar: data });
     }
 
     handleFilterSubmit(data) {
-        this.setState({filterBar: data});
+        this.setState({ filterBar: data });
         //apis.postDashboardDailySummaryReport(true).then((resp) => {
         //  this.setState({summaryReport: resp.data.content, projectName: data.projectName})
         //}).catch(()=> {
@@ -298,15 +261,14 @@ export default class SummaryReport extends Component {
     }
 
     render() {
-        const {loading,view, showAddPanel, params,createDate} = this.state;
-        const {userInstructions} = this.context;
+        const { loading, showAddPanel, createDate } = this.state;
         const panelIconStyle = showAddPanel ? 'angle double up icon' : 'angle double down icon';
         var summaryReport = this.state.summaryReport;
         summaryReport = summaryReport.split("<\/th>").join("").split("<\/td>").join("");
         if (loading) {
             return (
                 <Console.Content>
-                    <div className="ui form loading" style={{'marginTop': '36px'}}>
+                    <div className="ui form loading" style={{ 'marginTop': '36px' }}>
                     </div>
                 </Console.Content>
             )
@@ -317,7 +279,8 @@ export default class SummaryReport extends Component {
                 <div className="ui main tiny container" ref={c => this._el = c}>
                     <div className="ui clearing vertical segment">
                         <ButtonGroup className="left floated">
-                            <Button id="loadingRefresh" className="labeled icon" onClick={this.handleRefresh.bind(this)}>
+                            <Button id="loadingRefresh" className="labeled icon"
+                                    onClick={this.handleRefresh.bind(this)}>
                                 <i className="icon refresh"/>Refresh
                             </Button>
                         </ButtonGroup>
@@ -336,17 +299,16 @@ export default class SummaryReport extends Component {
 
                     <div className="ui vertical segment filterPanel"
                          ref={(c)=>this.$filterPanel = $(ReactDOM.findDOMNode(c))}>
-                        <i className="close link icon" style={{float:'right', marginTop: '-10px'}}
+                        <i className="close link icon" style={{ float: 'right', marginTop: '-10px' }}
                            onClick={this.handleToggleFilterPanel.bind(this)}/>
                         <FilterBar {...this.props} onSubmit={this.handleFilterSubmit.bind(this)}
-                                                   onChange={this.handleFilterChange.bind(this)}/>
-                        {userInstructions.clouddailysummary &&
-                        <Message dangerouslySetInnerHTML={{__html: summaryReport}}/>
+                                   onChange={this.handleFilterChange.bind(this)}/>
+                        <Message dangerouslySetInnerHTML={{ __html: summaryReport }}/>
                         }
                     </div>
 
                     <div key={Date.now()} className="ui vertical segment">
-                        {summaryData?<RenderSummaryReport summaryData={summaryData} createDate={createDate}/>:
+                        {summaryData ? <RenderSummaryReport summaryData={summaryData} createDate={createDate}/> :
                             <div>Report unavailable</div>
                         }
                     </div>
