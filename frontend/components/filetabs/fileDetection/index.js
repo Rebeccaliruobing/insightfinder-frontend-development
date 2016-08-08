c7193a89fbcdc5a2d9313167090357fd7907a68b
gaoqiang <qiang.gao@coalink.com>
2016-08-09 00:39:30 +0800

    init data filetabls

--- frontend/components/filetabs/fileDetection/index.js
+++ frontend/components/filetabs/fileDetection/index.js
@@ -1,15 +1,64 @@
 import React, {Component}    from 'react';
 import {
-  Modal, Console, ButtonGroup, Button, Dropdown, Accordion, Message
+    Modal, Console, ButtonGroup, Button, Dropdown, Accordion, Message
 }                           from '../../../artui/react/index';
 export default class FileDetection extends Component {
-    render(){
+    static contextTypes = {
+        userInstructions: React.PropTypes.object
+    };
+    render() {
+        let {userInstructions} = this.context;
         return (
-      <Console.Content>
-        <div style={{padding: 20}}>
-            FileDetection
-        </div>
-      </Console.Content>
-    );
+            <Console.Content>
+                <div className="ui main tiny container">
+                    <div className="ui clearing vertical segment">
+                    </div>
+                    <div className="ui vertical segment">
+                        <div className="ui grid two columns form">
+                            <div className="wide column">
+                                <h3>Anomaly detection and cause analysis</h3>
+                                <div className="field">
+                                        <label>Model Name</label>
+                                        <div className="">
+
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <Message dangerouslySetInnerHTML={{ __html: userInstructions.filedetection }}/>
+                    </div>
+                </div>
+
+            </Console.Content>
+        );
+    }
+
+    fileUploadRef(r) {
+        $(ReactDOM.findDOMNode(r))
+            .fileupload({
+                dataType: 'json',
+                url: `${baseUrl}cloudstorage/${store.get('userName')}/`,
+                sequentialUploads: true,
+                multipart: false
+            })
+            .bind('fileuploadadd', (e, data) => {
+                this.setState({settingLoading: true, projectName: data.files[0]['name']});
+            })
+            .bind('fileuploadprogress', (e, data) => {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                this.setState({settingLoading: true});
+            })
+            .bind('fileuploadfail', (e, data) => {
+                var resp = data.response().jqXHR.responseJSON;
+                this.setState({settingLoading: false});
+            })
+            .bind('fileuploaddone', (e, data) => {
+                var resp = data.response().jqXHR.responseJSON;
+                this.setState({
+                    projectHintMapFilename: data['data']['name'],
+                    data: Object.assign({}, this.state.data, {projectHintMapFilename: resp.filename}),
+                    settingLoading: false
+                });
+            });
     }
 }
--- frontend/components/filetabs/fileNewModel/index.js
+++ frontend/components/filetabs/fileNewModel/index.js
@@ -1,15 +1,109 @@
 import React, {Component}    from 'react';
 import {
-  Modal, Console, ButtonGroup, Button, Dropdown, Accordion, Message
+    Modal, Console, ButtonGroup, Button, Dropdown, Accordion, Message
 }                           from '../../../artui/react/index';
+import store from 'store';
+
+
+const baseUrl = window.API_BASE_URL || '/api/v1/';
+
 export default class FileNewModel extends Component {
-    render(){
+    static contextTypes = {
+        userInstructions: React.PropTypes.object
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            submitLoading: false,
+            loading: false
+        };
+    }
+
+    handleSharingChange(e) {
+        console.log(111);
+    }
+    handleSubmit(e){
+        console.log('submit');
+    }
+    render() {
+        let {userInstructions} = this.context;
         return (
-      <Console.Content>
-        <div style={{padding: 20}}>
-            FileNewModel
-        </div>
-      </Console.Content>
-    );
+            <Console.Content>
+                <div className="ui main tiny container">
+                    <div className="ui clearing vertical segment">
+                    </div>
+                    <div className="ui vertical segment">
+                        <div className="ui grid two columns form">
+                            <div className="wide column">
+                                <h3>Create a new model</h3>
+
+                                <div className="three fields fill">
+                                    <div className="field">
+                                        <label>Training Data</label>
+
+                                        <div className="ui button fileinput-button">
+                                            Training Data
+                                            <input type="file" name="file" ref={::this.fileUploadRef}/>
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label>Optional Filter Data</label>
+
+                                        <div className="ui button fileinput-button">
+                                            Optional Filter Data
+                                            <input type="file" name="file" ref={::this.fileUploadRef}/>
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label>Model Name:</label>
+
+                                        <div className="ui input">
+                                            <input type="text"
+                                                   value=""
+                                                   onChange={this.handleSharingChange.bind(this)}/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="ui field">
+                                    <Button className={cx('orange', {'loading': this.state.submitLoading})}
+                                            onClick={this.handleSubmit.bind(this)}>Submit</Button>
+                                </div>
+                            </div>
+                        </div>
+                        <Message dangerouslySetInnerHTML={{ __html: userInstructions.fileNewModel }}/>
+                    </div>
+                </div>
+            </Console.Content>
+        );
+    }
+
+    fileUploadRef(r) {
+        $(ReactDOM.findDOMNode(r))
+            .fileupload({
+                dataType: 'json',
+                url: `${baseUrl}cloudstorage/${store.get('userName')}/`,
+                sequentialUploads: true,
+                multipart: false
+            })
+            .bind('fileuploadadd', (e, data) => {
+                this.setState({settingLoading: true, projectName: data.files[0]['name']});
+            })
+            .bind('fileuploadprogress', (e, data) => {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                this.setState({settingLoading: true});
+            })
+            .bind('fileuploadfail', (e, data) => {
+                var resp = data.response().jqXHR.responseJSON;
+                this.setState({settingLoading: false});
+            })
+            .bind('fileuploaddone', (e, data) => {
+                var resp = data.response().jqXHR.responseJSON;
+                this.setState({
+                    projectHintMapFilename: data['data']['name'],
+                    data: Object.assign({}, this.state.data, {projectHintMapFilename: resp.filename}),
+                    settingLoading: false
+                });
+            });
+
     }
 }
