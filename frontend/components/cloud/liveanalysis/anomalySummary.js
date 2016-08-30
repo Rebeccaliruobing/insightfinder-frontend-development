0835e50940e7026e7eb961089ad3fa565748c2c7
gaoqiang <qiang.gao@coalink.com>
2016-08-30 23:06:09 +0800

    fix

--- frontend/components/cloud/liveanalysis/anomalySummary.js
+++ frontend/components/cloud/liveanalysis/anomalySummary.js
@@ -22,10 +22,24 @@ const HotMapCharts = React.createClass({
         heatMapX[heatMapX.length - 1] = (heatMapX[heatMapX.length - 1].split(']'))[0];
         heatMapY[0] = (heatMapY[0].split('['))[1];
         heatMapY[heatMapY.length - 1] = (heatMapY[heatMapY.length - 1].split(']'))[0];
-        console.log(heatMapX, heatMapY);
         var hours = heatMapY;
         var days = heatMapX;
-        var data = [[0,0,5],[0,1,1],[0,2,3],[0,3,1],[0,4,1],[0,5,1],[0,6,1],[0,7,1],[0,8,2],[1,0,7],[1,1,0],[1,2,0],[1,3,0],[1,4,0],[1,5,0],[1,6,0],[1,7,10],[1,8,6]];
+        let showData = [];
+        console.log(hours, days);
+        for(let i=0;i<days.length;i++){
+            for(let j=0;j<hours.length;j++){
+                if(flagShow){
+                    showData.push([i,j,0]);
+                }
+                else{
+                    let anomaly = anomalyHeatmapJson[days[i]]?anomalyHeatmapJson[days[i]][hours[j]]:0;
+                    showData.push([i,j,anomaly]);
+                }
+            }
+        }
+        console.log(showData);
+        //var data = [[0,0,5],[0,1,1],[0,2,3],[0,3,1],[0,4,1],[0,5,1],[0,6,1],[0,7,1],[0,8,2],[1,0,7],[1,1,0],[1,2,0],[1,3,0],[1,4,0],[1,5,0],[1,6,0],[1,7,10],[1,8,6]];
+        var data = showData;
 
         data = data.map(function (item) {
             return [item[1], item[0], item[2] || '-'];
