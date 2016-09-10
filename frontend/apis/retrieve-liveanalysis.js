3cd8f27ec82c06489b2bfd7e362b11cc4d75bb4e
Hao Fu <nullnoid@gmail.com>
2016-09-10 21:40:00 +0800

    convert live data to treemap data

--- frontend/apis/retrieve-liveanalysis.js
+++ frontend/apis/retrieve-liveanalysis.js
@@ -1,6 +1,76 @@
 import store from 'store';
+import _ from 'lodash';
 import getEndpoint from './get-endpoint';
 import DataParser from './data-parser';
+
+function buildTreemap(statistics, heapmap) {
+
+  // Create tree structure with instance => container => metric.
+  // If no container, instance => metric.
+
+  const ret = [];
+
+  // FIXME: instances, metrics is not a string instead of json array.
+  const insts = (statistics['instances'] || '').replace(/[\[\]]/g, '').split(',');
+  const newInsts = (statistics['newInstances'] || '').replace(/[\[\]]/g, '').split(',');
+  const metrics = (statistics['metrics'] || '').replace(/[\[\]]/g, '').split(',');
+
+  _.forEach(insts, (inst) => {
+
+    const anomalies = heapmap[inst] || {};
+
+    const names = inst.split('_');
+    const isContainer = names.length > 1;
+    const iname = isContainer ? names[1] : names[0];
+    const cname = isContainer ? names[0] : '';
+
+    const children = _.map(metrics, (m) => {
+      const mn = m.trim();
+      const val = parseFloat(anomalies[mn]);
+      return {
+        id: mn,
+        type: 'metric',
+        active: true,
+        name: mn,
+        value: _.isFinite(val) ? val : 0.0,
+      };
+    });
+
+    if (isContainer) {
+      const container = {
+        type: 'container',
+        name: cname,
+        active: !_.find(newInsts, i => i === inst),
+        instance: inst,
+        children,
+      };
+
+      // Find the instance by name, and add container to children.
+      let instance = _.find(ret, o => o.name === iname);
+      if (!instance) {
+        ret.push({
+          type: 'instance',
+          name: iname,
+          instance: inst,
+          active: true,
+          children: [container],
+        });
+      } else {
+        instance.children.push(container);
+      }
+    } else {
+      ret.push({
+        instance: inst,
+        type: 'instance',
+        active: !_.find(newInsts, i => i === inst),
+        name: iname,
+        children,
+      });
+    }
+  });
+
+  return ret;
+}
 
 /**
  * Api to retrieve project's live analysis data.
@@ -26,18 +96,23 @@ const retrieveLiveAnalysis = (projectName, modelType, pvalue, cvalue) => {
 
         try {
           const data = resp.data;
+          const statistics = data['instanceMetricJson'] || {};
+          const heatmap = data['anomalyHeatmapJson'] || {};
+
           const parser = new DataParser(data);
           parser.getSummaryData();
 
           const ret = {};
-          ret['statistics'] = data['instanceMetricJson'] || {};
+          ret['statistics'] = statistics;
           ret['summary'] = parser.summaryData || {};
+          ret['incidentsTreeMap'] = buildTreemap(statistics, heatmap);
 
           // TODO: Get incidents list object or array;
           ret['incidents'] = {};
 
           resolve(ret);
         } catch (e) {
+          console.log(e);
           reject(`Data Error: ${e.message}`)
         }
       } else {
--- frontend/components/incidents/treemap.js
+++ frontend/components/incidents/treemap.js
@@ -1,14 +1,37 @@
 import React, {PropTypes as T} from 'react';
+import * as d3 from 'd3';
+import Faux from 'react-faux-dom';
 
-const IncidentsTreeMap = ({incidents}) => {
-  console.log(incidents);
-  return (
-    <div className="">treemap</div>
-  )
-};
+const IncidentsTreeMap = React.createClass({
+  mixins: [
+    Faux.mixins.core,
+    Faux.mixins.anim,
+  ],
 
-IncidentsTreeMap.propTypes = {
-  incidents: T.object,
-};
+  getInitialState () {
+    return {
+      chart: 'loading...'
+    }
+  },
+
+  componentDidMount () {
+
+    const {data} = this.props;
+    const faux = this.connectFauxDOM('div.treemap', 'chart')
+
+    d3.select(faux)
+      .html('Hello World!')
+  },
+
+  render () {
+    const {data} = this.props;
+    console.log(data);
+    return (
+      <div className="incidents treemap">
+        {this.state.chart}
+      </div>
+    )
+  }
+});
 
 export default IncidentsTreeMap;
