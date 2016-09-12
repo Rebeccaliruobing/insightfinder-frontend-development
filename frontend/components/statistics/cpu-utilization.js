1a28243ffdd5ead43a65e634059ef8da824f3e85
Hao Fu <nullnoid@gmail.com>
2016-09-12 14:25:04 +0800

    adjust cpu usage color based on value

--- frontend/components/statistics/cpu-utilization.js
+++ frontend/components/statistics/cpu-utilization.js
@@ -2,13 +2,14 @@ import React, {PropTypes as T} from 'react';
 
 const CPUUtilization = ({average, duration='24h' }) => {
   const averageText = average !== undefined ? average.toFixed(1).toString() : '-';
+  const valueStyle = _.isFinite(average) && average < 50 ? 'value error' : 'value';
   return (
     <div className="ui statistic two wide column">
       <div>
         <span className="title">CPU Utilization</span>
         <span className="meta">{duration}</span>
       </div>
-      <div className="value">{`${averageText}%`}</div>
+      <div className={valueStyle}>{`${averageText}%`}</div>
       <div className="label">Avg</div>
     </div>
   )
