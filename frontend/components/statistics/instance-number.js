3f98025ce61feeabbaf30f4c7291889dbef2b8b5
Hao Fu <nullnoid@gmail.com>
2016-09-12 14:14:10 +0800

    remove container statistics if not exists

--- frontend/components/statistics/instance-number.js
+++ frontend/components/statistics/instance-number.js
@@ -2,16 +2,21 @@ import React, {PropTypes as T} from 'react';
 import _ from 'lodash';
 
 const InstanceNumber = ({total, containerTotal}) => {
+
   const instanceText = _.isFinite(total) ? total.toString() : '-';
   const containerText = _.isFinite(containerTotal) ? containerTotal.toString() : '0';
+  const hasContainer = _.isFinite(containerTotal) && containerTotal > 0;
+
+  const value = hasContainer ? `${containerText}/${instanceText}` : instanceText;
+  const label = hasContainer ? 'Container / Instance' : 'Instance';
 
   return (
     <div className='ui statistic two wide column'>
       <div>
         <span className="title">{`Num of Instances`}</span>
       </div>
-      <div className="value">{`${containerText}/${instanceText}`}</div>
-      <div className="label">Container / Instance</div>
+      <div className="value">{value}</div>
+      <div className="label">{label}</div>
     </div>
   )
 };
