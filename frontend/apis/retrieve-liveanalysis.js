74e0284aad775dd742f97ba7ae40e126874b457f
Hao Fu <nullnoid@gmail.com>
2016-10-01 21:16:10 +0800

    begin buildTimeTable

--- frontend/apis/retrieve-liveanalysis.js
+++ frontend/apis/retrieve-liveanalysis.js
@@ -99,6 +99,25 @@ export function buildTreemap(projectName, incidentName, statistics, anomaliesLis
   };
 }
 
+function buildTimeTable(startTime, endTime, predicatedEndTime, incidents) {
+  const table = [];
+
+  if (!startTime && !endTime && !predicatedEndTime && !incidents) {
+    return table;
+  }
+
+  const start = moment(startTime);
+  const end = moment(endTime);
+  const pend = moment(predicatedEndTime);
+
+  // 1 day, 1* 24 * 60 = 24 * 6 * 10m
+  // 3 day, 3 * 24 * 60 = 24 * 6 * 30mins
+  // 7 day, 7 * 24 * 60 = 28 * 6 * 60mins
+  // 14 day, 14 * 24 * 60 = 28 * 6 * 120min
+
+  return table;
+}
+
 /**
  * Api to retrieve project's live analysis data.
  * @param pname: The name of the project
@@ -143,6 +162,8 @@ export function retrieveLiveAnalysis(projectName, modelType, pvalue, cvalue, ver
           ret['latestDataTimestamp'] = latestTimestamp;
           ret['incidentsTreeMap'] = buildTreemap(projectName, projectName+" (1d)", statistics, heatmap);
           ret['incidents'] = incidentList;
+          ret['incidentsTimeTable'] = buildTimeTable(
+            data.startTimestamp, data.endTimestamp, data.predictedEndTime, incidentList);
 
           resolve(ret);
         } catch (e) {
