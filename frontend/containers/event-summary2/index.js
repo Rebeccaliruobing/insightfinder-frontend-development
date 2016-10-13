532842c440dd06c1be08d68192c50c38db8a1fd3
Hao Fu <nullnoid@gmail.com>
2016-10-13 23:13:37 +0800

    fix zindex conflict with topbar

--- frontend/containers/event-summary2/index.js
+++ frontend/containers/event-summary2/index.js
@@ -160,10 +160,10 @@ class EventSummary2 extends Component {
     return (
       <Console.Content className={ loading ? 'ui form loading' : ''}>
         <div className="ui main tiny container" style={{ minHeight: '100%', display: loading && 'none' }}>
-          <div className="ui right aligned vertical inline segment" style={{zIndex: 1000}}>
+          <div className="ui right aligned vertical inline segment" style={{zIndex: 200}}>
             <div className="field">
               <label style={{ fontWeight: 'bold' }}>Project Name:</label>
-              <LiveProjectSelection value={projectName} onChange={this.handleProjectChange} style={{width: 200}}/>
+              <LiveProjectSelection value={projectName} onChange={this.handleProjectChange} style={{minWidth: 200}}/>
             </div>
             <div className="field">
               <label style={{ fontWeight: 'bold' }}>End date:</label>
