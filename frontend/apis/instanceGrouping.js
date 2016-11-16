91f3676f1b6152807f5d05c0948c586f0cb24e1e
Chao Huang <chaoh818@gmail.com>
2016-11-15 21:53:49 -0500

    add file

---
+++ frontend/apis/instanceGrouping.js
@@ -0,0 +1,69 @@
+import store from 'store';
+import _ from 'lodash';
+import getEndpoint from './get-endpoint';
+import DataParser from './data-parser';
+import moment from 'moment';
+
+export function loadInstanceGrouping(projectName) {
+  const userName = store.get('userName');
+  const token = store.get('token');
+
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      type: 'POST',
+      url: getEndpoint('instanceGrouping', 1),
+      data: $.param({ userName, token, projectName, operation:'load' }),
+      beforeSend: function (request) {
+        request.setRequestHeader("Accept", 'application/json');
+      }
+    }).done(function (resp) {
+      if (resp.success) {
+        try {
+          const data = resp.data;
+          resolve(data);
+        } catch (e) {
+          console.log(e);
+          reject(`Data Error: ${e.message}`)
+        }
+      } else {
+        reject(resp.message);
+      }
+    }).fail(function (resp) {
+      console.log(resp);
+      reject(`Server Error: ${resp.status}\n${resp.statusText}`);
+    });
+  });
+}
+
+
+export function saveInstanceGrouping(projectName, groupingJson) {
+  const userName = store.get('userName');
+  const token = store.get('token');
+
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      type: 'POST',
+      url: getEndpoint('instanceGrouping', 1),
+      data: $.param({ userName, token, projectName, groupingJson, operation:'save' }),
+      beforeSend: function (request) {
+        request.setRequestHeader("Accept", 'application/json');
+      }
+    }).done(function (resp) {
+      if (resp.success) {
+        try {
+          const data = resp.data;
+          resolve(data);
+        } catch (e) {
+          console.log(e);
+          reject(`Data Error: ${e.message}`)
+        }
+      } else {
+        reject(resp.message);
+      }
+    }).fail(function (resp) {
+      console.log(resp);
+      reject(`Server Error: ${resp.status}\n${resp.statusText}`);
+    });
+  });
+}
+
