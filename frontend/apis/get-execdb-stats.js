c5f3bb8536dac715c418d0d2bf1e70ce9e06d384
Shane O <shaneo@insightfinder.com>
2016-11-25 00:52:05 -0500

    Adding new exec db files.

---
+++ frontend/apis/get-execdb-stats.js
@@ -0,0 +1,101 @@
+import store from 'store';
+import _ from 'lodash';
+import getEndpoint from './get-endpoint';
+import DataParser from './data-parser';
+import moment from 'moment';
+
+export function getExecDBStatisticsData(projectName, endDate, modelType, duration) {
+  const userName = store.get('userName');
+  const token = store.get('token');
+
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      type: 'POST',
+      url: getEndpoint('SystemsStatsDataServlet', 1),
+      data: $.param({ userName, token, projectName, endDate,
+											modelType, duration, operation:'get' }),
+      beforeSend: function (request) {
+        request.setRequestHeader("Accept", 'application/json');
+      }
+    }).done(function (resp) {
+      if (resp.success) {
+
+        try {
+          const data = resp.data;
+          resolve(data);
+        } catch (e) {
+          console.log(e);
+          reject(`Data Error: ${e.message}`)
+        }
+      } else {
+        reject(resp.message);
+      }
+    }).fail(function (resp) {
+      console.log(resp);
+      reject(`Server Error: ${resp.status}\n${resp.statusText}`);
+    });
+  });
+}
+
+export function retrieveAppNames(projectName) {
+  const userName = store.get('userName');
+  const token = store.get('token');
+
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      type: 'POST',
+      url: getEndpoint('appForecast', 1),
+      data: $.param({ userName, token, projectName, operation:'list' }),
+      beforeSend: function (request) {
+        request.setRequestHeader("Accept", 'application/json');
+      }
+    }).done(function (resp) {
+      if (resp.success) {
+
+        try {
+          const data = resp.data;
+          resolve(data);
+        } catch (e) {
+          console.log(e);
+          reject(`Data Error: ${e.message}`)
+        }
+      } else {
+        reject(resp.message);
+      }
+    }).fail(function (resp) {
+      console.log(resp);
+      reject(`Server Error: ${resp.status}\n${resp.statusText}`);
+    });
+  });
+}
+
+export function retrieveAppMetricForecastData(projectName, appName, interval) {
+  const userName = store.get('userName');
+  const token = store.get('token');
+
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      type: 'POST',
+      url: getEndpoint('appForecast', 1),
+      data: $.param({ userName, token, projectName, appName, interval, operation:'timeseries' }),
+      beforeSend: function (request) {
+        request.setRequestHeader("Accept", 'application/json');
+      }
+    }).done(function (resp) {
+      if (resp.success) {
+        try {
+          const data = resp.data;
+          resolve(data);
+        } catch (e) {
+          console.log(e);
+          reject(`Data Error: ${e.message}`)
+        }
+      } else {
+        reject(resp.message);
+      }
+    }).fail(function (resp) {
+      console.log(resp);
+      reject(`Server Error: ${resp.status}\n${resp.statusText}`);
+    });
+  });
+}
