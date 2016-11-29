601cf55f9f20f994b03ed96863cfc565ab2d8e2d
Shane O <shaneo@insightfinder.com>
2016-11-29 17:51:08 -0500

    This shit works -- finally!

--- frontend/components/statistics/index.js
+++ frontend/components/statistics/index.js
@@ -1,4 +1,4 @@
 export ProjectStatistics from './project';
 export ThreeValueBox from './three-value-box';
 export HourlyHeatmap from './hourly-heatmap';
-//export Top5Grid from './top-5-grid';
+export Top5Grid from './top-5-grid';
--- frontend/components/statistics/top-5-grid.js
+++ frontend/components/statistics/top-5-grid.js
@@ -2,18 +2,18 @@ import React, {PropTypes as T} from 'react';
 import _ from 'lodash';
 import Reactable from 'reactable';
 
-export default class Top5Grid extends React.Component {
-	static defaultProps = { title:"Top 5 Projects", 
+var Table = Reactable.Table,
+		Thead = Reactable.Thead,
+		Th = Reactable.Th,
+		Tr = Reactable.Tr,
+		Td = Reactable.Td;
+
+const Top5Grid = ({title, parentType, parentName, duration }) => {
+	const defaultProps = { title:"Top 5 Projects", 
 										parentType:"Project", 
 										parentName:"All", 
 										duration:'7' };
 				
-	var (Table = Reactable.Table,
-			Thead = Reactable.Thead,
-			Th = Reactable.Th,
-			Tr = Reactable.Tr,
-			Td = Reactable.Td);
-
   return (
     <div>
       <Table>
@@ -126,7 +126,7 @@ Top5Grid.propTypes = {
 	title: T.string,
   parentType: T.string,
   parentName: T.string,
-  duration: T.number,
+  duration: T.number
 };
 
 export default Top5Grid;
