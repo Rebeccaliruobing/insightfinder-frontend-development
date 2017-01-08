5b5611a9f9e65a98e9df1c786d1cbd6464e9e9f8
Chao Huang <chaoh818@gmail.com>
2017-01-08 11:28:24 -0500

    eb update

--- frontend/containers/executive-dashboard/normalize-stats.js
+++ frontend/containers/executive-dashboard/normalize-stats.js
@@ -20,19 +20,50 @@ const normalizeStats = (stats = {}, orderBy) => {
     const stat = o[1];
 
     // create sum group to be put on top row
-    let sumStat = {"previous":{"totalAnomalyEventCount":0,"avgDailyAnomalyScore":0,"totalAnomalyDuration":0},"current":{"totalAnomalyEventCount":0,"avgDailyAnomalyScore":0,"totalAnomalyDuration":0},"predicted":{"totalAnomalyEventCount":0,"avgDailyAnomalyScore":0,"totalAnomalyDuration":0}};
+    let sumStat = {"previous":{"totalAnomalyEventCount":0,"avgDailyAnomalyScore":0,"totalAnomalyDuration":0,"AvgCPUUtilization":0,"AvgInstanceUptime":0},"current":{"totalAnomalyEventCount":0,"avgDailyAnomalyScore":0,"totalAnomalyDuration":0},"predicted":{"totalAnomalyEventCount":0,"avgDailyAnomalyScore":0,"totalAnomalyDuration":0,"AvgCPUUtilization":0,"AvgInstanceUptime":0}};
 
+    let statsCountCurrent = 0;
+    let statsCountPrevious = 0;
     _.each(stat, function (s, i) {
-      if(s['previous'] && s['previous']['totalAnomalyDuration']) sumStat['previous']['totalAnomalyDuration'] += s['previous']['totalAnomalyDuration'];
-      if(s['previous'] && s['previous']['totalAnomalyEventCount']) sumStat['previous']['totalAnomalyEventCount'] += s['previous']['totalAnomalyEventCount'];
-      if(s['previous'] && s['previous']['avgDailyAnomalyScore']) sumStat['previous']['avgDailyAnomalyScore'] += s['previous']['avgDailyAnomalyScore'];
-      if(s['current'] && s['current']['totalAnomalyDuration']) sumStat['current']['totalAnomalyDuration'] += s['current']['totalAnomalyDuration'];
-      if(s['current'] && s['current']['totalAnomalyEventCount']) sumStat['current']['totalAnomalyEventCount'] += s['current']['totalAnomalyEventCount'];
-      if(s['current'] && s['current']['avgDailyAnomalyScore']) sumStat['current']['avgDailyAnomalyScore'] += s['current']['avgDailyAnomalyScore'];
-      if(s['predicted'] && s['predicted']['totalAnomalyDuration']) sumStat['predicted']['totalAnomalyDuration'] += s['predicted']['totalAnomalyDuration'];
-      if(s['predicted'] && s['predicted']['totalAnomalyEventCount']) sumStat['predicted']['totalAnomalyEventCount'] += s['predicted']['totalAnomalyEventCount'];
-      if(s['predicted'] && s['predicted']['avgDailyAnomalyScore']) sumStat['predicted']['avgDailyAnomalyScore'] += s['predicted']['avgDailyAnomalyScore'];
+      if(s['previous']){
+        if(s['previous']['totalAnomalyDuration']) sumStat['previous']['totalAnomalyDuration'] += s['previous']['totalAnomalyDuration'];
+        if(s['previous']['totalAnomalyEventCount']) sumStat['previous']['totalAnomalyEventCount'] += s['previous']['totalAnomalyEventCount'];
+        if(s['previous']['avgDailyAnomalyScore']) sumStat['previous']['avgDailyAnomalyScore'] += s['previous']['avgDailyAnomalyScore'];
+        if(s['previous']['AvgInstanceUptime']) sumStat['previous']['AvgInstanceUptime'] += s['previous']['AvgInstanceUptime'];
+        if(s['previous']['AvgCPUUtilization']) {
+          sumStat['previous']['AvgCPUUtilization'] += s['previous']['AvgCPUUtilization'];
+          statsCountPrevious += 1;
+        } 
+      }
+      if(s['current']){
+        if(s['current']['totalAnomalyDuration']) sumStat['current']['totalAnomalyDuration'] += s['current']['totalAnomalyDuration'];
+        if(s['current']['totalAnomalyEventCount']) sumStat['current']['totalAnomalyEventCount'] += s['current']['totalAnomalyEventCount'];
+        if(s['current']['avgDailyAnomalyScore']) sumStat['current']['avgDailyAnomalyScore'] += s['current']['avgDailyAnomalyScore'];
+        if(s['current']['AvgInstanceUptime']) sumStat['current']['AvgInstanceUptime'] += s['current']['AvgInstanceUptime'];
+        if(s['current']['AvgCPUUtilization']) {
+          sumStat['current']['AvgCPUUtilization'] += s['current']['AvgCPUUtilization'];
+          statsCountCurrent += 1;
+        } 
+      }
+      if(s['predicted']){
+        if(s['predicted']['totalAnomalyDuration']) sumStat['predicted']['totalAnomalyDuration'] += s['predicted']['totalAnomalyDuration'];
+        if(s['predicted']['totalAnomalyEventCount']) sumStat['predicted']['totalAnomalyEventCount'] += s['predicted']['totalAnomalyEventCount'];
+        if(s['predicted']['avgDailyAnomalyScore']) sumStat['predicted']['avgDailyAnomalyScore'] += s['predicted']['avgDailyAnomalyScore'];
+      }
     });
+
+    if(sumStat['current'] && sumStat['current']['AvgCPUUtilization']){
+      if(statsCountCurrent==0){
+        sumStat['current']['AvgCPUUtilization'] = undefined;  
+      }
+      sumStat['current']['AvgCPUUtilization'] = sumStat['current']['AvgCPUUtilization']/statsCountCurrent;
+    }
+    if(sumStat['previous'] && sumStat['previous']['AvgCPUUtilization']){
+      if(statsCountPrevious==0){
+        sumStat['previous']['AvgCPUUtilization'] = undefined;  
+      }
+      sumStat['previous']['AvgCPUUtilization'] = sumStat['previous']['AvgCPUUtilization']/statsCountPrevious;
+    }
 
     // Use the all stat as the group stat.
     // const { All: allStats, ...subStats } = stat;
