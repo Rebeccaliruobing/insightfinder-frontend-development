5b846d510c1bded17370132c40265e024cc61061
Hao Fu <hao@insightfinder.com>
2017-06-02 11:28:19 -0400

    fix predicated event highlight issue

--- frontend/artui/react/elements/dygraph/index.js
+++ frontend/artui/react/elements/dygraph/index.js
@@ -9,6 +9,7 @@
 import React, { Component, PropTypes as T } from 'react';
 import classNames from 'classnames';
 import R from 'ramda';
+import moment from 'moment';
 import { autobind } from 'core-decorators';
 import { DygraphDefaultProps, spreadDygraphProps } from './options';
 
@@ -44,75 +45,83 @@ class Dygraph extends Component {
     const left = g.toDomXCoord(x_start);
     const right = g.toDomXCoord(x_end);
     canvas.fillStyle = '#88bbee';
-    canvas.fillRect(left, area.y, right - left , 12);
+    canvas.fillRect(left, area.y, right - left, 12);
   }
 
-  _highlight_period(canvas, area, g, x_start, x_end, val, latestDataTimestamp = undefined, enableTriangleHighlight=false, isMissingValue = false) {
-      // val between 0-green to 1-yellow to 10-red (logarithmic)
-      var canvas_left_x = g.toDomXCoord(x_start);
-      var canvas_right_x = g.toDomXCoord(x_end);
-      var canvas_height = 12;
-      let area_y = area.y;
-      var canvas_width = Math.max(canvas_right_x - canvas_left_x, 5);
-      var rcolor, gcolor, bcolor = 0;
-      let gcolorMax = 205;
-      let sign = 0;
-      if(val < 0){
-          val = -val;
-          sign = -1;
-      } else if (val > 0){
-          sign = 1;
-      }
-      if (val <= 1) {
-          if (val < 0) val = 0;
-          rcolor = Math.floor(255 * val);
-          gcolor = gcolorMax;
-      } else {
-          if (val > 10) val = 10;
-          rcolor = 255;
-          gcolor = Math.floor(gcolorMax - (val - 1) / 9 * gcolorMax);
-      }
-      canvas.fillStyle = "rgba(" + rcolor.toString() + "," + gcolor.toString() + "," + bcolor.toString() + ",1.0)";
-      if(!enableTriangleHighlight || isMissingValue || sign==0){
-          canvas.fillRect(canvas_left_x, area.y, canvas_width, 12);
-      }else if(sign<0){
-          canvas.beginPath();
-          canvas.moveTo(canvas_left_x,area.y);
-          canvas.lineTo(canvas_left_x+canvas_width/2,area.y+12);
-          canvas.lineTo(canvas_left_x+canvas_width,area.y);
-          canvas.fill();
-      } else if (sign>0){
-          canvas.beginPath();
-          canvas.moveTo(canvas_left_x,area.y+12);
-          canvas.lineTo(canvas_left_x+canvas_width/2,area.y);
-          canvas.lineTo(canvas_left_x+canvas_width,area.y+12);
-          canvas.fill();
-      }
+  _highlight_period(canvas, area, g, x_start, x_end, val, latestDataTimestamp = undefined, enableTriangleHighlight = false, isMissingValue = false) {
+    // val between 0-green to 1-yellow to 10-red (logarithmic)
+    var canvas_left_x = g.toDomXCoord(x_start);
+    var canvas_right_x = g.toDomXCoord(x_end);
+    var canvas_height = 12;
+    let area_y = area.y;
+    var canvas_width = Math.max(canvas_right_x - canvas_left_x, 5);
+    var rcolor, gcolor, bcolor = 0;
+    let gcolorMax = 205;
+    let sign = 0;
+    if (val < 0) {
+      val = -val;
+      sign = -1;
+    } else if (val > 0) {
+      sign = 1;
+    }
+    if (val <= 1) {
+      if (val < 0) val = 0;
+      rcolor = Math.floor(255 * val);
+      gcolor = gcolorMax;
+    } else {
+      if (val > 10) val = 10;
+      rcolor = 255;
+      gcolor = Math.floor(gcolorMax - (val - 1) / 9 * gcolorMax);
+    }
+    canvas.fillStyle = "rgba(" + rcolor.toString() + "," + gcolor.toString() + "," + bcolor.toString() + ",1.0)";
+    if (!enableTriangleHighlight || isMissingValue || sign == 0) {
+      canvas.fillRect(canvas_left_x, area.y, canvas_width, 12);
+    } else if (sign < 0) {
+      canvas.beginPath();
+      canvas.moveTo(canvas_left_x, area.y);
+      canvas.lineTo(canvas_left_x + canvas_width / 2, area.y + 12);
+      canvas.lineTo(canvas_left_x + canvas_width, area.y);
+      canvas.fill();
+    } else if (sign > 0) {
+      canvas.beginPath();
+      canvas.moveTo(canvas_left_x, area.y + 12);
+      canvas.lineTo(canvas_left_x + canvas_width / 2, area.y);
+      canvas.lineTo(canvas_left_x + canvas_width, area.y + 12);
+      canvas.fill();
+    }
   }
 
   @autobind
   handleUnderlayCallback(canvas, area, g) {
-
-    const {highlights, enableTriangleHighlight, latestDataTimestamp,
-       highlightStartTime, highlightEndTime, data,
+    const { highlights, enableTriangleHighlight, latestDataTimestamp,
+      highlightStartTime, highlightEndTime, data,
     } = this.props;
 
     // Draw predicated range if latestDataTimestamp is specified.
-    if (latestDataTimestamp && data.length > 0) {
+    // Ignore the latestDataTimestamp, and use the current time?
+    // const latestTime = latestDataTimestamp;
+    const latestTime = moment().valueOf();
+    if (latestTime && data.length > 0) {
       const begin = data[0][0].valueOf();
-      const start = Math.max(latestDataTimestamp, begin);
-      const x = g.toDomXCoord(start);
-      const y = area.y;
-      const w = (area.w + area.x) - x;
-      const h = area.h;
-      canvas.fillStyle = this.predicatedRangeFillStyle;
-      canvas.fillRect(x, y, w, h);
+      const last = data[data.length - 1][0].valueOf();
+      if (latestTime < last) {
+        const start = Math.max(latestTime, begin);
+        const x = g.toDomXCoord(start);
+        const y = area.y;
+        const w = (area.w + area.x) - x;
+        const h = area.h;
+        if (w > 0) {
+          canvas.fillStyle = this.predicatedRangeFillStyle;
+          canvas.fillRect(x, y, w, h);
+        }
+      }
     }
 
     // If has highlights, draw the highlight bar.
     if (highlights) {
       highlights.forEach(o => {
-        this._highlight_period(canvas, area, g, o.start, o.end, o.val, this.props['latestDataTimestamp'],enableTriangleHighlight, o.isMissingValue);
+        this._highlight_period(canvas, area, g, o.start, o.end, o.val,
+        this.props.latestDataTimestamp, enableTriangleHighlight, o.isMissingValue);
       });
     }
 
@@ -195,11 +204,11 @@ class Dygraph extends Component {
   }
 
   render() {
-    const {className, style} = this.props;
+    const { className, style } = this.props;
     const classes = classNames('ui', className, 'graph');
 
     return (
-      <div ref={c=> this._el = c} className={classes} style={style} />
+      <div ref={c => this._el = c} className={classes} style={style} />
     );
   }
 }
