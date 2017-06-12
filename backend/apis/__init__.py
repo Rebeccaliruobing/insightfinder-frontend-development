a8222f5d5dcf474269b12329c8416f7e9bbfe2ed
Hao Fu <hao@insightfinder.com>
2017-06-12 21:33:13 +0800

    remove mock backend files

--- backend/apis/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    本模块包括api路由定义及对应的方法。
-"""
-
-
-def init_app(app):
-    from ..core import api
--- backend/core/prototype/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .clorem import clorem
-from .images import avatars, wireframes
-
-__all__ = ['clorem', 'avatars', 'wireframes']
