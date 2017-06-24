8338e65d11ef031cfa6d658f34cfa4606936b478
Hao Fu <hao@insightfinder.com>
2017-06-24 01:26:43 -0400

    fix model picking issue for group

--- frontend/components/settings/threshold/model.js
+++ frontend/components/settings/threshold/model.js
@@ -2,7 +2,11 @@ import React, { PropTypes as T } from 'react';
 import { autobind } from 'core-decorators';
 import R from 'ramda';
 import moment from 'moment';
-import { getProjectModels, pickProjectModel, removeProjectModel } from '../../../apis/projectSettings';
+import {
+  getProjectModels,
+  pickProjectModel,
+  removeProjectModel,
+} from '../../../apis/projectSettings';
 import { Tile } from '../../../src/lib/fui/react';
 import ModelTile from './ModelTile';
 
@@ -10,12 +14,12 @@ class ModelSettings extends React.Component {
   static propTypes = {
     projectName: T.string.isRequired,
     instanceGroup: T.string,
-  }
+  };
 
   static defaultProps = {
     projectName: '',
     instanceGroup: '',
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -31,37 +35,41 @@ class ModelSettings extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.projectName !== this.props.projectName ||
-      nextProps.instanceGroup !== this.props.instanceGroup) {
+    if (
+      nextProps.projectName !== this.props.projectName ||
+      nextProps.instanceGroup !== this.props.instanceGroup
+    ) {
       this.reloadData(nextProps.projectName, nextProps.instanceGroup);
     }
   }
 
-  @autobind
-  reloadData(projectName, instanceGroup) {
+  @autobind reloadData(projectName, instanceGroup) {
     if (projectName) {
-      this.setState({
-        loading: true,
-      }, () => {
-        getProjectModels(projectName, instanceGroup).then((data) => {
-          const models = data.modelKeys || [];
-          const pickedModelKeys = R.map(
-            m => m.modelKey,
-            R.filter(m => m.pickedFlag)(models));
+      this.setState(
+        {
+          loading: true,
+        },
+        () => {
+          getProjectModels(projectName, instanceGroup)
+            .then((data) => {
+              const models = data.modelKeys || [];
+              const pickedModelKeys = R.map(m => m.modelKey, R.filter(m => m.pickedFlag)(models));
 
-          this.setState({
-            models,
-            pickedModelKeys,
-            loading: false,
-          });
-        }).catch(() => {
-          this.setState({
-            models: [],
-            pickedModelKeys: [],
-            loading: false,
-          });
-        });
-      });
+              this.setState({
+                models,
+                pickedModelKeys,
+                loading: false,
+              });
+            })
+            .catch(() => {
+              this.setState({
+                models: [],
+                pickedModelKeys: [],
+                loading: false,
+              });
+            });
+        },
+      );
     }
   }
 
@@ -71,83 +79,95 @@ class ModelSettings extends React.Component {
     this.reloadData(projectName, instanceGroup);
   }
 
-  @autobind
-  handleSelectProjectModel(projectName, instanceGroup, key) {
+  @autobind handleSelectProjectModel(projectName, instanceGroup, key) {
     this.setState({
       pickedModelKeys: [key],
     });
   }
 
-  @autobind
-  handlePickProjectModel(projectName, instanceGroup, key) {
+  @autobind handlePickProjectModel(projectName, instanceGroup, key) {
     const { models } = this.state;
     const pickedModel = R.find(m => m.modelKey === key, models);
     const { startTimestamp, endTimestamp, modelKey } = pickedModel;
     const modelKeyObj = {
-      startTimestamp, endTimestamp, modelKey,
+      startTimestamp,
+      endTimestamp,
+      modelKey,
     };
 
-    this.setState({
-      loading: true,
-    }, () => {
-      pickProjectModel(projectName, instanceGroup, JSON.stringify(modelKeyObj))
-        .then(() => {
+    this.setState(
+      {
+        loading: true,
+      },
+      () => {
+        pickProjectModel(projectName, instanceGroup, JSON.stringify(modelKeyObj)).then(() => {
           this.setState({
             loading: false,
             pickedModelKeys: [key],
           });
         });
-    });
+      },
+    );
   }
 
-  @autobind
-  handleRemoveProjectModel(projectName, instanceGroup, key) {
+  @autobind handleRemoveProjectModel(projectName, instanceGroup, key) {
     const { models } = this.state;
     const pickedModel = R.find(m => m.modelKey === key, models);
     const { startTimestamp, endTimestamp, modelKey } = pickedModel;
     const modelKeyObj = {
-      startTimestamp, endTimestamp, modelKey,
+      startTimestamp,
+      endTimestamp,
+      modelKey,
     };
 
-    this.setState({
-      loading: true,
-    }, () => {
-      removeProjectModel(projectName, instanceGroup, JSON.stringify(modelKeyObj))
-        .then(() => {
+    this.setState(
+      {
+        loading: true,
+      },
+      () => {
+        removeProjectModel(projectName, instanceGroup, JSON.stringify(modelKeyObj)).then(() => {
           this.reloadData(projectName, instanceGroup);
         });
-    });
+      },
+    );
   }
 
   render() {
     const { projectName, instanceGroup } = this.props;
-    const { models, pickedModelKeys, loading } = this.state;
+    const { pickedModelKeys, loading } = this.state;
+    const group = instanceGroup.replace('Env:', '');
+    const models = R.filter((m) => {
+      const metrics = (m.metricNameList || '[]').slice(1, -1).split(',');
+      if (metrics.length > 0) {
+        const metric = metrics[0];
+        if (
+          (group.toLowerCase() === 'all' || group.toLowerCase() === '') &&
+          metric.indexOf('[]:') >= 0
+        ) {
+          return true;
+        } else if (metric.indexOf(`[${group}]:`) >= 0) {
+          return true;
+        }
+        return false;
+      }
 
+      return false;
+    }, this.state.models);
     const pickedModels = R.filter(m => R.find(R.equals(m.modelKey), pickedModelKeys), models);
     const pickedModel = pickedModels.length > 0 ? pickedModels[0] : null;
 
     return (
       <Tile isAncestor className={`model-settings ${loading ? 'ui form loading' : ''}`}>
-        <Tile
-          isParent isVertical size={3}
-          style={{ padding: 0, marginRight: 10 }}
-        >
+        <Tile isParent isVertical size={3} style={{ padding: 0, marginRight: 10 }}>
           <h4>Model Details:</h4>
           {pickedModel &&
             <div style={{ paddingBottom: '1em', paddingRight: '1em' }}>
               {`You picked model 
               ${moment(pickedModel.startTimestamp).format('MM/DD HH:mm')}-
               ${moment(pickedModel.endTimestamp).format('MM/DD HH:mm')}
-              to be used ${
-                pickedModel.pickedExpiry ?
-                'till ' + moment(pickProjectModel.pickedExpiry).format('MM/DD HH:mm') :
-                'for the next 24 hours'}.`
-              }
-            </div>
-          }
-          {pickedModels.length === 0 &&
-            <h4 style={{ marginTop: 0 }}>No picked model</h4>
-          }
+              to be used ${pickedModel.pickedExpiry ? `till ${moment(pickProjectModel.pickedExpiry).format('MM/DD HH:mm')}` : 'for the next 24 hours'}.`}
+            </div>}
+          {pickedModels.length === 0 && <h4 style={{ marginTop: 0 }}>No picked model</h4>}
           {pickedModels.map(m => (
             <ModelTile key={m.modelKey} model={m} big projectName={projectName} />
           ))}
@@ -160,12 +180,11 @@ class ModelSettings extends React.Component {
             values.
           </div>
           <Tile isParent isFluid style={{ padding: 0 }}>
-            {models.length === 0 &&
-              <h4>No model available</h4>
-            }
+            {models.length === 0 && <h4>No model available</h4>}
             {models.map((m, idx) => (
               <ModelTile
-                key={idx} model={m}
+                key={idx}
+                model={m}
                 picked={R.find(R.equals(m.modelKey), pickedModelKeys)}
                 projectName={projectName}
                 instanceGroup={instanceGroup}
