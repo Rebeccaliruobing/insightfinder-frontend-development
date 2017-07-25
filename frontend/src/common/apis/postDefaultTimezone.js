b68bedbdd966561ea4e7a7b035acccf3ab9dc3a5
Hao Fu <hao@insightfinder.com>
2017-07-25 22:42:05 +0800

    add timezoneoffset

--- frontend/src/common/apis/postDefaultTimezone.js
+++ frontend/src/common/apis/postDefaultTimezone.js
@@ -4,14 +4,18 @@
  * Copyright InsightFinder Inc., 2017
  * *****************************************************************************
  **/
+import moment from 'moment';
+import momenttz from 'moment-timezone';
 import type { Credentials } from '../types';
 import getEndpoint from './getEndpoint';
 import fetchPost from './fetchPost';
 
 const postDefaultTimezone = (credentials: Credentials, defaultTimezone: String) => {
+  const timezoneOffset = momenttz.tz.zone(defaultTimezone).offset(moment.utc());
   return fetchPost(getEndpoint('system/updateTimezone'), {
     ...credentials,
     timezoneString: defaultTimezone,
+    timezoneOffset,
   }).then(d => {
     return d;
   });
